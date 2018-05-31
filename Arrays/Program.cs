6cfa8d0d4b63f3fd89da0c4d8b0afd0f0df62400
ravisankar426 <ravisankar426@gmail.com>
2018-05-31 02:12:20 -0400

    .

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -37,28 +37,27 @@ namespace Arrays
             QuickSelectResult result = new QuickSelectResult();
             result.stop = false;
 
-            Console.WriteLine(QuickSelect(arr, 0, arr.Length - 1, 3,result).number.ToString());
+            Console.WriteLine(QuickSelect(arr, 0, arr.Length - 1, 2,result).number.ToString());
 
             Console.Read();
         }
 
         public static QuickSelectResult QuickSelect(int[] arr, int left, int right, int k,QuickSelectResult result)
         {
-            if (left < right)
+            if (left <= right)
             {
                 int pi = Partition(arr, left, right);
 
                 if (pi + 1 == k)
                 {
-                    result = new QuickSelectResult();
                     result.stop = true;
                     result.number = arr[pi];
                 }
 
-                if(!result.stop)                
-                    result=QuickSelect(arr, left, pi - 1, k,result);
                 if (!result.stop)
-                    result =QuickSelect(arr, pi + 1, right, k,result);                
+                    result = QuickSelect(arr, left, pi - 1, k, result);
+                if (!result.stop)
+                    result = QuickSelect(arr, pi + 1, right, k, result);                                
 
                 return result;
             }
